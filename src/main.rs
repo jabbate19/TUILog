bc290b87679df17bf6575d4695233e7e50c78668
Joe Abbate <josephabbateny@gmail.com>
2023-12-28 20:17:35 -0500

    ADIF Writing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use adif::{AdifRecord, AdifType, AdifHeader, AdifFile};
 use anyhow::{anyhow, Result};
 use chrono::{NaiveDateTime, Utc};
 use cursive::menu::Tree;
@@ -10,8 +11,13 @@ use cursive_table_view::{TableView, TableViewItem};
 use rand::Rng;
 use rusqlite::{params, Connection, Result as SqliteResult, MAIN_DB};
 use std::cmp::Ordering;
+use std::collections::hash_map::RandomState;
+use std::fs::File;
+use std::io::Write;
 use std::sync::{Arc, Mutex};
 use std::{fs, path::PathBuf};
+use homedir::get_my_home;
+use indexmap::map::IndexMap;
 
 #[derive(Copy, Clone, PartialEq, Eq, Hash)]
 enum LogbookColumn {
@@ -50,6 +56,30 @@ struct Logbook {
     frequency: String,
     mode: String,
     comments: String,
+}
+
+#[derive(Clone, Debug)]
+struct OperatorConfig {
+    call: String,
+    grid: String,
+    cqz: String,
+    ituz: String,
+    dxcc: String,
+    cont: String,
+}
+
+#[derive(Clone, Debug)]
+struct LogbookExt {
+    timestamp: NaiveDateTime,
+    call: String,
+    rsttx: String,
+    rstrx: String,
+    band: String,
+    frequency: String,
+    mode: String,
+    power: String,
+    comments: String,
+    operator: OperatorConfig,
 }
 
 impl TableViewItem<LogbookColumn> for Logbook {
@@ -112,11 +142,14 @@ fn add_log(s: &mut Cursive, connection: Arc<Mutex<Connection>>) {
     let rstrx = s
         .call_on_name("rstrx", |view: &mut EditView| view.get_content())
         .unwrap();
+    let power = s
+        .call_on_name("power", |view: &mut EditView| view.get_content())
+        .unwrap();
     let comments = s
         .call_on_name("comments", |view: &mut EditView| view.get_content())
         .unwrap();
     if let Ok(conn) = connection.lock() {
-        let mut stmt = "INSERT INTO logs (timestamp, call, rsttx, rstrx, band, frequency, mode, comments, operator_config) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        let mut stmt = "INSERT INTO logs (timestamp, call, rsttx, rstrx, band, frequency, mode, power, comments, operator_config) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         let timestamp = Utc::now()
             .naive_utc()
             .format("%Y-%m-%d %H:%M:%S")
@@ -132,6 +165,7 @@ fn add_log(s: &mut Cursive, connection: Arc<Mutex<Connection>>) {
                 band,
                 frequency,
                 mode,
+                power,
                 comments,
                 operator_config,
             ),
@@ -262,6 +296,7 @@ fn select_mode(s: &mut Cursive) {
 }
 
 fn new_log(s: &mut Cursive, connection: Arc<Mutex<Connection>>) -> Result<()> {
+    s.pop_layer();
     s.add_layer(
         Dialog::around(
             LinearLayout::vertical()
@@ -299,6 +334,10 @@ fn new_log(s: &mut Cursive, connection: Arc<Mutex<Connection>>) -> Result<()> {
                         .child(
                             Dialog::around(EditView::new().with_name("rstrx").fixed_width(5))
                                 .title("RST RX"),
+                        )
+                        .child(
+                            Dialog::around(EditView::new().with_name("power").fixed_width(5))
+                                .title("Power (Watts)"),
                         ),
                 )
                 .child(
@@ -317,6 +356,126 @@ fn new_log(s: &mut Cursive, connection: Arc<Mutex<Connection>>) -> Result<()> {
         )
         .title("Add Log"),
     );
+    Ok(())
+}
+
+fn export (s: &mut Cursive, connection: Arc<Mutex<Connection>>) -> Result<()> {
+    let start_timestamp = s
+        .call_on_name("start_timestamp", |view: &mut EditView| {
+            let content = view.get_content();
+            if content.len() > 0 {
+                return Some(NaiveDateTime::parse_from_str(content.as_str(), "%Y-%m-%d %H:%M:%S"));
+            } else {
+                return None;
+            }
+        })
+        .unwrap();
+    if let Some(Err(start_err)) = start_timestamp {
+        return Err(anyhow!("Could not parse start timestamp: {}", start_err));
+    }
+    let end_timestamp = s
+        .call_on_name("end_timestamp", |view: &mut EditView| {
+            let content = view.get_content();
+            if content.len() > 0 {
+                return Some(NaiveDateTime::parse_from_str(content.as_str(), "%Y-%m-%d %H:%M:%S"));
+            } else {
+                return None;
+            }
+        })
+        .unwrap();
+    if let Some(Err(end_err)) = end_timestamp {
+        return Err(anyhow!("Could not parse start timestamp: {}", end_err));
+    }
+    let export_path = s
+        .call_on_name("export_path", |view: &mut EditView| {
+            let content = view.get_content();
+            if content.len() > 0 {
+                return Some(content);
+            } else {
+                return None;
+            }
+        })
+        .unwrap();
+    let logs = if let Ok(connection) = connection.lock() {
+        let mut stmt = connection.prepare(
+            "SELECT timestamp, logs.call, rsttx, rstrx, band, frequency, mode, power, comments, operatorconfig.call, grid, cqz, ituz, dxcc, cont  FROM logs JOIN operatorconfig ON logs.operator_config = operatorconfig.id ORDER BY timestamp DESC;",
+        ).unwrap();
+        let mut logs: Vec<AdifRecord> = Vec::new();
+        let log_out = stmt.query_map((), |row| {
+            Ok(LogbookExt {
+                timestamp: row.get(0)?,
+                call: row.get(1)?,
+                rsttx: row.get(2)?,
+                rstrx: row.get(3)?,
+                band: row.get(4)?,
+                frequency: row.get(5)?,
+                mode: row.get(6)?,
+                power: row.get(7)?,
+                comments: row.get(8)?,
+                operator: OperatorConfig {
+                    call: row.get(9)?,
+                    grid: row.get(10)?,
+                    cqz: row.get(11)?,
+                    ituz: row.get(12)?,
+                    dxcc: row.get(13)?,
+                    cont: row.get(14)?,
+                }
+            })
+        }).unwrap();
+        for log in log_out {
+            let log = log.unwrap();
+            if let Some(Ok(start)) = start_timestamp {
+                if log.timestamp < start {
+                    continue;
+                }
+            }
+            if let Some(Ok(end)) = end_timestamp {
+                if log.timestamp > end {
+                    continue;
+                }
+            }
+            let mut map: IndexMap<&str, AdifType> = IndexMap::new();
+            map.insert("CALL", AdifType::Str(log.call));
+            map.insert("QSO_DATE", AdifType::Str(log.timestamp.format("%Y%m%d").to_string()));
+            map.insert("TIME_ON", AdifType::Str(log.timestamp.format("%H%M%S").to_string()));
+            map.insert("FREQ", AdifType::Str(log.frequency.clone()));
+            map.insert("BAND", AdifType::Str(log.band.clone()));
+            map.insert("FREQ_RX", AdifType::Str(log.frequency));
+            map.insert("BAND_RX", AdifType::Str(log.band));
+            map.insert("COMMENT", AdifType::Str(log.comments));
+            if log.mode == "USB" || log.mode == "LSB" {
+                map.insert("MODE", AdifType::Str("SSB".to_string()));
+                map.insert("SUBMODE", AdifType::Str(log.mode));
+            } else {
+                map.insert("MODE", AdifType::Str(log.mode.clone()));
+            }
+            map.insert("MY_GRIDSQUARE", AdifType::Str(log.operator.grid));
+            map.insert("STATION_CALLSIGN", AdifType::Str(log.operator.call.clone()));
+            map.insert("CQZ", AdifType::Str(log.operator.cqz));
+            map.insert("ITUZ", AdifType::Str(log.operator.ituz));
+            map.insert("DXCC", AdifType::Str(log.operator.dxcc));
+            map.insert("CONT", AdifType::Str(log.operator.cont));
+            map.insert("OPERATOR", AdifType::Str(log.operator.call));
+            map.insert("RST_SENT", AdifType::Str(log.rsttx));
+            map.insert("RST_RCVD", AdifType::Str(log.rstrx));
+            map.insert("TX_PWR", AdifType::Str(log.power));
+            logs.push(map.into());
+        }
+        logs
+    } else {
+        return Err(anyhow!("Could not lock connection"));
+    };
+    let mut map: IndexMap<&str, AdifType> = IndexMap::new();
+    map.insert("PROGRAMVERSION", AdifType::Str("1.0.0".to_string()));
+    map.insert("PROGRAMID", AdifType::Str("TUILOG".to_string()));
+    let header: AdifHeader = map.into();
+    let file_out = AdifFile {
+        header,
+        body: logs,
+    };
+    let mut file = File::create(export_path.ok_or(anyhow!("No export path received"))?.to_string())?;
+    file.write(file_out.serialize().map_err(|_| anyhow!("Failed to serialize data"))?.as_bytes())?;
+    s.pop_layer();
     Ok(())
 }
 
@@ -360,37 +519,26 @@ fn make_table(s: &mut Cursive, connection: Arc<Mutex<Connection>>) -> Result<()>
     };
 
     table.set_items(logs);
+    let export_conn = connection.clone();
+    s.pop_layer();
     s.add_layer(
         Dialog::around(
             LinearLayout::horizontal()
                 .child(
                     LinearLayout::vertical()
-                        .child(Button::new("Filter", |s| {
+                        .child(Button::new("Export", move |s| {
+                            let export_conn = export_conn.clone();
                             s.add_layer(
                                 Dialog::around(
                                     LinearLayout::vertical()
                                         .child(DummyView)
-                                        .child(EditView::new().fixed_width(10))
+                                        .child(Dialog::around(EditView::new().with_name("start_timestamp").fixed_width(10)).title("Start Timestamp"))
+                                        .child(Dialog::around(EditView::new().with_name("end_timestamp").fixed_width(10)).title("End Timestamp"))
+                                        .child(Dialog::around(EditView::new().with_name("export_path").fixed_width(10)).title("File Path"))
                                         .child(DummyView)
-                                        .child(Button::new("Submit", |s| {
-                                            s.pop_layer();
-                                        })),
+                                        .child(Button::new("Submit", move |s| export(s, export_conn.clone()).unwrap())),
                                 )
                                 .title("Filter"),
-                            )
-                        }))
-                        .child(Button::new("Export", |s| {
-                            s.add_layer(
-                                Dialog::around(
-                                    LinearLayout::vertical()
-                                        .child(DummyView)
-                                        .child(EditView::new().fixed_width(10))
-                                        .child(DummyView)
-                                        .child(Button::new("Submit", |s| {
-                                            s.pop_layer();
-                                        })),
-                                )
-                                .title("Export"),
                             )
                         }))
                         .align_center(),
@@ -405,15 +553,15 @@ fn make_table(s: &mut Cursive, connection: Arc<Mutex<Connection>>) -> Result<()>
 }
 
 fn main() -> Result<()> {
-    // This doesn't work
-    let target_path = PathBuf::from("~/tuilog/tuilog.db");
-    let connection = Connection::open(target_path)?;
+    let mut homepath = get_my_home().unwrap().unwrap();
+    homepath.push(".tuilog/tuilog.db");
+    let connection = Connection::open(homepath)?;
     let query = "
         CREATE TABLE IF NOT EXISTS operatorconfig (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, call TEXT, grid TEXT, cqz TEXT, ituz TEXT, dxcc TEXT, cont TEXT);
     ";
     connection.execute(query, ())?;
     let query = "
-        CREATE TABLE IF NOT EXISTS logs (id INTEGER PRIMARY KEY AUTOINCREMENT, timestamp TEXT, call TEXT, rsttx TEXT, rstrx TEXT, band TEXT, frequency TEXT, mode TEXT, comments TEXT, operator_config INTEGER NOT NULL REFERENCES operatorConfig(id));
+        CREATE TABLE IF NOT EXISTS logs (id INTEGER PRIMARY KEY AUTOINCREMENT, timestamp TEXT, call TEXT, rsttx TEXT, rstrx TEXT, band TEXT, frequency TEXT, mode TEXT, power TEXT, comments TEXT, operator_config INTEGER NOT NULL REFERENCES operatorConfig(id));
     ";
     connection.execute(query, ())?;
 
@@ -441,6 +589,9 @@ fn main() -> Result<()> {
     );
 
     siv.add_global_callback(Key::Esc, |s| s.select_menubar());
+
+    siv.add_layer(Dialog::text("TUILog v1.0.0").title("TUILog"));
+
     siv.run();
 
     Ok(())
